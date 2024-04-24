92af4916abd3546c9bee7cfbc55eea56865ab101
taniacruzz <tanicruz112@gmail.com>
2024-04-24 20:03:20 -0300

    first commit

---
+++ src/test/java/com/fatec/prova/ProvaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.prova;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
